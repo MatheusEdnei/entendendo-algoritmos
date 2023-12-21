68d80b1460ccd48cd3e3e858769ab44a757f79ed
MatheusEdnei <ednei880@gmail.com>
2023-12-21 11:43:20 -0300

    feat(hash-table): finish the implementation of hash-table

--- hash-tables/implements-hash-table/HashTable.js
+++ hash-tables/implements-hash-table/HashTable.js
@@ -16,26 +16,59 @@ export default class HashTable {
   // Adiciona um novo elemento na hash table
   set(key, value) {
     const index = this._hash(key);
-    this.table[index] = [key, value];
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        // Atualiza o valor de uma chave que já existe
+        if (this.table[index][i][0] === key) {
+          this.table[index][i][1] = value;
+          return;
+        }
+      }
+      // Se a posição estiver preenchida e a chave for diferente, cria um novo array
+      this.table[index].push([key, value]);
+    } else {
+      this.table[index] = [];
+      this.table[index].push([key, value]);
+    }
     this.size++;
   }
 
   // Obtém um elemento da hash table
   get(key) {
     const index = this._hash(key);
-    return this.table[index];
+    if (this.table[index]) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[index][i][0] === key) {
+          const value = this.table[index][i][1];
+          return value;
+        }
+      }
+    }
+    return undefined;
   }
 
   // Remove um elemento da hash table
   remove(key) {
     const index = this._hash(key);
-
     if (this.table[index] && this.table[index].length) {
-      this.table[index] = [];
-      this.size--;
-      return true;
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index].splice(i, 1);
+          this.size--;
+          return true;
+        }
+      }
     } else {
       return false;
     }
+  }
+
+  display() {
+    this.table.forEach((values, index) => {
+      const chainedValues = values.map(
+        ([key, value]) => `[ ${key}: ${value} ]`
+      );
+      console.log(`${index}: ${chainedValues}`);
+    })
   }
 }
--- hash-tables/implements-hash-table/index.js
+++ hash-tables/implements-hash-table/index.js
@@ -4,11 +4,11 @@ const ht = new HashTable();
 ht.set("Canada", 300);
 ht.set("France", 100);
 ht.set("Spain", 110);
+ht.set("ǻ", 100);
 
-console.log(ht.get("Canada")[1]); // [ 'Canada', 300 ]
+console.log(ht.get("Canada")); // [ 'Canada', 300 ]
 console.log(ht.get("France")); // [ 'France', 100 ]
 console.log(ht.get("Spain")); // [ 'Spain', 110 ]
+console.log(ht.get("ǻ")); // [ 'ǻ', 100 ]   
 
-console.log(ht.remove("Spain")); // true
-console.log(ht.get("Spain")); // undefined
-
+ht.display();
